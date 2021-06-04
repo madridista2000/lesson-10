9fd707bfe4e7b4c010a2e824fd9385dedd06a384
madridista2000 <lukamoqia@gmail.com>
2021-06-04 21:39:35 +0400

    java

---
+++ script.js
@@ -0,0 +1,85 @@
+// let cars = ['audi','bmw', 'lexus', 'volkswagen', 'porsche','mercedes',]; 
+// for (let x = 1; x <= 4; x++) {
+    
+//     console.log(cars[x]);
+    
+// }
+
+
+
+
+// let names = ['giorgi','vaxo','eka','oto', 'beqa','cotne','ia','elene','saba'];
+// for (let y = 0; y < names.length; y++ ){
+//     if (names[y].length > 3) {
+//     console.log(names[y]);
+//     }
+// }
+
+
+
+// let numbers = [ 15, 546, 989, 14, 36, 17, 129, 39, 15, 6, 4, 878, 23, 68, 78, 1549, 65, 1, 30, 2000]
+
+// for (let n = 0; n < numbers.length; n ++) {
+//     if (numbers[n] % 2 == 0) {
+//         console.log(numbers[n]);
+    
+//     }
+// }
+
+
+// let numbers = [ 15, 546, 989, 14, 36, 17, 129, 39, 15, 6, 4, 878, 23, 68, 78, 1549, 65, 1, 30, 2000]
+
+// for (let n = 0; n < numbers.length; n ++) {
+//     if (numbers[n] % 2 == 1) {
+//         console.log(numbers[n]);
+    
+//     }
+// }
+
+
+
+// let numbers = [15, 67, 100, 5, 7, [80, 500] ];
+
+
+//     console.log(numbers[5][1]);
+
+
+// let person = {
+//     firstname: "john",
+//     lastname: "doe",
+//     age: "50",
+//     eyecolor: "blue"
+// };
+
+// console.log(person.eyecolor)
+
+
+// let citys = ['rustavi', 'tbilisi', 'batumi', 'borjomi', 'kutaisi'];
+
+// for (let x of citys) {
+//     console.log(x);
+// }
+
+
+// let names = ['ilia', 'dato','akaki', 'saba', 'nika'];
+
+
+//      if (names.length == 5 && names[names.length - 1] == 'nika') {
+//          console.log("მასივის რაოდენობა: 5 და ბოლო ელემენტია ნიკა");
+//     } else {
+//         console.log('error');
+//     }
+
+
+// let names = ['sandro', 'ilia', 'dato','akaki', 'saba', 'nika'];
+
+
+// if (names.length == 7 || names[0] == 'sandro') {
+//     console.log("მასივის რაოდენობა: 7 და პირველი ელემენტია სანდრო");
+// } else {
+//     console.log('error');
+// }
+
+
+რას დააუბრუნებს 6 === “6”? (true ან false) ახსენით რატო? 
+დააუბრუნებს false  რადგან სხვადასხვა ტიპებია
